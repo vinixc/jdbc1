3d3d5e31588f9b34c9703a0dc7b76d5711e4c2b6
Vinicius Carvalho <vinicius.tecbcc@gmail.com>
2020-01-31 13:40:20 -0300

    recupera dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,8 +11,30 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
-		Connection con = DB.getConnection();
-		DB.closeConnection();
+		Connection con = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			con = DB.getConnection();
+			
+			st = con.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
 	}
 
 }
